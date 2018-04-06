e470ec32129a08861a035fedcff7de41194079c0
GSBhub <buthker.2@wright.edu>
2018-04-06 08:19:27 -0400

    Bit more work on the disassemble method
    Still need to flush it out with all the switch statements, added in the arg array. Will probably also need individual methods for parsing out the opcodes.

--- asm/asm_m7700.c
+++ asm/asm_m7700.c
@@ -4,6 +4,28 @@
 
 #define OPS 15 // placeholder for number of ops, will implement later
 
+const char *instruction_set[] = {
+
+	"ADC", "AND", "ASL", "BCC", "BCS", "BEQ", "BIT", "BMI", "BNE",  "BPL", "BRA",
+	"BRK", "BRL", "BVC", "BVS", "CLC", "CLD", "CLI", "CLV", "CMP",  "COP", "CPX",
+	"CPY", "DEA", "DEC", "DEX", "DEY", "EOR", "INA", "INC", "INX",  "INY", "JML",
+	"JMP", "JSL", "JSR", "LDA", "LDX", "LDY", "LSR", "MVN", "MVP",  "NOP", "ORA",
+	"PEA", "PEI", "PER", "PHA", "PHT", "PHD", "PHK", "PHP", "PHX",  "PHY", "PLA",
+	"PLT", "PLD", "PLP", "PLX", "PLY", "CLP", "ROL", "ROR", "RTI",  "RTL", "RTS",
+	"SBC", "SEC", "SED", "SEI", "SEP", "STA", "STP", "STX", "STY",  "STZ", "TAX",
+	"TAY", "TAS", "TAD", "TDA", "TRB", "TSB", "TSA", "TSX", "TXA",  "TXS", "TXY",
+	"TYA", "TYX", "WIT", "WDM", "XBA", "XCE", "MPY", "DIV", "MPYS", "DIVS", "RLA",
+	"EXTS","EXTZ","LDT", "LDM", "UNK", "SEB", "SEM", "CLM", "STB",  "LDB", "ADCB",
+	"SBCB","EORB","TBX", "CMPB","INB", "DEB", "TXB", "TYB", "LSRB", "ORB", "CLB",
+	"BBC", "BBS", "TBY", "ANDB","PUL", "PSH", "PLB", "XAB", "PHB",  "TBS", "TBD",
+	"TDB"
+};
+
+static ut32 getInstruction(const ut8 *data) {
+
+
+}
+
 static const  char *ops[OPS * 2] = {
 
 	//TODO: include ops/params
@@ -12,13 +34,24 @@ static const  char *ops[OPS * 2] = {
 /* Main disassembly func */
 static int disassemble(RAsm *a, RAsmOp *op, ut8 *buf, ut64 len) {
 
-	char arg[32];
+	char arg[16];
 
 	int idx = (buf[0] & 0x0f) * 2;
 
 	op->size = 2;
 
-}
+	// TODO: implement ut16 and ut8 differentiation for instructions, get from buff
+	ut16 instruction = get16Instruction(buf);
+	ut8 instruction = get8Instruction(buf);
+	OpCode opcode = GET_OPCODE(instruction);
+
+	switch (opcode) {
+	
+		//TODO: Begin implementation of switch for operators
+	
+	};
+
+};
 
 /* Structure of exported functions/data (used in R2) */
 RAsmPlugin r_asm_plugin_m7700 = {
@@ -40,4 +73,3 @@ struct r_lib_struct_t radare_plugin = {
 	.data = &r_asm_plugin_m7700
 };
 #endif
-
---
+++ asm/m7700.h
@@ -0,0 +1,15 @@
+
+
+/*
+	m7700 instructions
+
+	m and x flags select between word, byte operations for each instruction
+	This allows for some instructions to be 8 bit, and some to be 16 bit. 
+
+	Each instruction also has an addressing mode that can fall into one of 28 different categories. 
+
+	There are a grand total of 103 instructions for the base m7700, each has a few permutations for each of the
+	addressing/bit length mode above.
+
+
+ */
